433abb765c1e706988d1a9fc33fd3e04e14aa0dd
Amir Kakon <amir.kakon@mesh-systems.com>
2024-01-25 10:46:51 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import { lightThemeOptions, darkThemeOptions } from './theme';
-import { ThemeProvider, useMediaQuery, createTheme } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { HomePage, AboutPage } from './pages';
-import { Header } from './layout';
+import logo from "./logo.svg";
+import "./App.css";
+import { lightThemeOptions, darkThemeOptions } from "./theme";
+import { ThemeProvider, useMediaQuery, createTheme } from "@mui/material";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { HomePage, AboutPage } from "./pages";
+import { Header } from "./layout";
 
 const App = () => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const [theme, setTheme] = useState(
-    createTheme(prefersDarkMode ? darkThemeOptions : lightThemeOptions)
+    // createTheme(prefersDarkMode ? darkThemeOptions : lightThemeOptions)
+    createTheme(lightThemeOptions)
   );
 
   useEffect(() => {
-    setTheme(createTheme(prefersDarkMode ? darkThemeOptions : lightThemeOptions));
+    setTheme(
+      // createTheme(prefersDarkMode ? darkThemeOptions : lightThemeOptions)
+      createTheme(lightThemeOptions)
+    );
   }, [prefersDarkMode]);
 
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Header />
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Routes>
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="*" element={<Navigate to="/home" replace />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
       </Router>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -7,13 +7,13 @@ import Button from '@mui/material/Button';
 
 const Header = () => {
   return (
-    <AppBar position="static">
+    <AppBar color="primary" position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           My App
         </Typography>
-        <Button color="inherit" component={RouterLink} to="/">Home</Button>
-        <Button color="inherit" component={RouterLink} to="/about">About</Button>
+        <Button color="secondary" component={RouterLink} to="/">Home</Button>
+        <Button color="secondary" component={RouterLink} to="/about">About</Button>
       </Toolbar>
     </AppBar>
   );
